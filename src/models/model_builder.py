c8c3a4f64dc57f4b6470ead5996355ab1faa05e0
jasper <jason.senjia@outlook.com>
2026-01-20 12:09:58 -0500

    add time model, but needs resolution adjustment

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -1,3 +1,5 @@
+from torch import nn
+import timm
 
 from src.models.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from src.models.resnext import ResNeXt29_2x64d, ResNeXt29_4x64d, ResNeXt29_8x64d, ResNeXt29_32x4d
@@ -17,3 +19,14 @@ def create_model(model_name: str, **model_args):
     model = model_func(**model_args)
     print (f"Initialized Model {model_name}.")
     return model 
+
+def timm_model(model_name: str, image_size: int, num_classes: int, drop_rate:float=.3, drop_path_rate:float=.1) -> nn.Module:
+    model = timm.create_model(
+        model_name, 
+        pretrained=True,
+        num_classes=num_classes,
+        drop_rate=drop_rate,
+        drop_path_rate=drop_path_rate,
+    )
+    print (type(model))
+    return model
